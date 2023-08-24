3fc9a5cfae7b9adf7cdd3deabdc3eada594525a3
Gustavo Berny <pelotas.berny93@gmail.com>
2023-08-23 21:52:14 -0300

    tela Home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+import axios from "axios";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    const response = await axios.get("http://localhost:5000/products");
+
+    if (response.status === 200) {
+      setData(response.data);
+    }
+  };
+
+  console.log("data=>", data);
   return (
-    <div>
-      <h2>Noissss</h2>
+    <div style={{ marginTop: "150px" }}>
+      <table className="styled-table">
+        <thead>
+          <tr>
+            <th style={{ textAlign: "center" }}>No. Product</th>
+            <th style={{ textAlign: "center" }}>Name Product</th>
+            <th style={{ textAlign: "center" }}>Price</th>
+            <th style={{ textAlign: "center" }}>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => {
+            return (
+              <tr key={index}>
+                <th scope="row">{index + 1}</th>
+                <td>{item.dscproduct}</td>
+                <td>{item.price}</td>
+                <td>
+                  <Link to={`/update/${item.id}`}>
+                    <button className="btn btn-edit">Edit</button>
+                  </Link>
+                  <button className="btn btn-delete">Delete</button>
+                  <Link to={`/view/${item.id}`}>
+                    <button className="btn btn-view">View</button>
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
