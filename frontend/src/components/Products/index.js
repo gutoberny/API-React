5063162303b0deeeed2af45fec7a48d178d8d450
Gustavo Berny <pelotas.berny93@gmail.com>
2023-08-24 15:54:39 -0300

    instalando redux

---
+++ frontend/src/components/Products/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class index extends Component {
+  state = {};
+  render() {
+    const { products } = this.state;
+    return;
+  }
+}
+
+export default index;
