c9282b0cb781d1a7cad64534d81fc055edc0a700
Gustavo Berny <pelotas.berny93@gmail.com>
2023-08-25 11:14:03 -0300

    removido useCallback

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   fetchProducts,
